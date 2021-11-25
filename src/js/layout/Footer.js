f5d24654d652a72c908b6ae9687d00998d6fd9a3
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-25 14:38:09 -0500

    Add Footer of the page

---
+++ src/js/layout/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Footer = () => {
+
+    return (
+        <div className="footer">
+            <p className="footer-text">M3: Implantação de E-commerce VTEX</p>
+        </div>
+    );
+};
