d6d02db9eb604e38fde924d7e6b5c38f58714c0e
lucas3982 <lucasbarroca777@hotmail.com>
2021-11-25 23:49:52 -0300

    Pueba Tecnica

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-console.log("Dev m3");
+let productos = [];
+let total = 0;
+
+function add(img, precio) {
+    console.log(img, precio);
+    productos.push(img),
+    total = total + precio;
+    document.getElementById('carrito').innerHTML = `Pagar $${total}`
+}
+
+function carrito() {
+    
+    window.alert(productos.join(", \n"));
+}
+
+
