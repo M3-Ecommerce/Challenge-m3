6df4ad3fadf44f7ecc9f40e0c861a6352dd79697
enigma <camilo.murcia.pimentel@gmail.com>
2021-11-26 00:04:49 -0500

    Add filter by color,size,price on the resposive way

--- src/js/layout/OrderMobile.js
+++ src/js/layout/OrderMobile.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import { AsideBar } from './AsideBar';
+
+//apply filter by color, size and price 
+export const OrderMobile = (props) => {
+    
+    const {
+        handleOrderFilterMob,
+        clearButton,
+        colors,
+        sizes,
+        prices,
+        products,
+        handleColorClick,
+        handleFilterReset,
+        handleSizeClick,
+        handlePriceClick,
+    } = props
 
 
-export const OrderMobile = () => {
+
     return (
-        <div>
-            <h1>OrderMobile</h1>
+        <div className="mobile-filter">
+            <div className="mobile-header">
+                <p className="mobile-header_title">FILTRAR</p>
+                <button 
+                    className="mobile-close" 
+                    onClick={() => {handleOrderFilterMob(false)}}
+                >
+                    X
+                </button>
+            </div>
+            <hr className="mobile-header-breakline"/>
+            <div>
+                <AsideBar
+                    clearButton={clearButton}
+                    colors={colors}
+                    sizes={sizes}
+                    prices={prices}
+                    products={products}
+                    handleColorClick={(currentColor) => {handleColorClick(currentColor)}}
+                    handleFilterReset={handleFilterReset}
+                    handleSizeClick={(selectedSize) => {handleSizeClick(selectedSize)}}
+                    handlePriceClick={(selectedPrice) => {handlePriceClick(selectedPrice)}}
+                />
+            </div>
         </div>
-    )
-}
+    );
+};
